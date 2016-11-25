151117d27f32ace2c8a637561af56b62cd1f9a79
Nico Jansen <jansennico@gmail.com>
2016-11-25 13:36:00 +0100

    Working inc deeplinking/routing

--- election-app/election-app.js
+++ election-app/election-app.js
@@ -1,14 +1,26 @@
 Polymer({
     is: 'election-app',
+    behaviors: [Polymer.NeonAnimationRunnerBehavior],
+    observers: ['_viewChanged(routeChildData.id)'],
     properties: {
         hasWinner: {
             type: Boolean,
             computed: 'determineHasWinner(votes.*)'
-        }
+        },
+        animationConfig: {
+            value: function () {
+                return {
+                    vote: [{
+                        name: 'scale-down-animation',
+                        node: this.$.candidateGrid
+                    }]
+                }
+            }
+        },
     },
     vote: function (event) {
-        console.log('voted', event.model.item);
         this.push('votes', new Vote(event.model.item, new Date().toLocaleTimeString()));
+        this.playAnimation('vote');
     },
     sortVotes: function (a, b) {
         return a.time < b.time;
@@ -27,5 +39,24 @@ Polymer({
     },
     determineHasWinner: function () {
         return this.votes.length > 10;
-    }
+    },
+    _showInfo: function (event) {
+        this.set('selectedCandidate', event.model.item);
+        // this.$.pages.selected = 'details';
+        this.set('route.path', '/details/' + this.candidates.indexOf(event.model.item));
+    },
+    _voteCount: function () {
+        return this.votes.filter(v => v.candidate === this.selectedCandidate).length;
+    },
+    _goBack: function () {
+        this.$.pages.selected = 'vote';
+        this.$.candidateGrid.notifyResize(); // notify to refresh... don't ask
+    },
+    _viewChanged: function (view) {
+        setTimeout(function () {
+            if (this.routeChildData.id) {
+                this.selectedCandidate = this.candidates[parseInt(this.routeChildData.id)];
+            }
+        }.bind(this));
+    },
 });
--- readme.md
+++ readme.md
@@ -18,6 +18,8 @@ Web cmp    -----------------------------------
 What WCF is to communcation is what angular is to front end
 What HTTP REST is to communication is what Polymer is to front end
 
+## Creating elements
+
 * Quickstart is not easy. Choose between Yeoman, PolymerCLI, Bower
 * Data binding is polymer specific. So components build for polymer cannot be 
 reused in XTAG/SkateJS
@@ -47,3 +49,25 @@ reused in XTAG/SkateJS
       </template>
       ```
       Might look like an angular expression, but sure as hell is not!
+
+## Using elements
+
+* Neon animation using `Behavior`s
+  * `behaviors: [ Polymer.NeonAnimationRunnerBehavior ]` 
+  * works with `animationConfig`
+    * `easing`, `name`, `node`
+  * works with `listeners`
+    * `'neon-animation-finish'`: update the *end-state*
+
+## Routing
+
+* Using `app-elements`
+* Using deeplinking
+  * use `<app-location>` to translate (2 way bind) url and expose as complex `route` object
+  * use `<app-route>` to translate a `route` object to correct `routeData`
+  * Use `<iron-pages>` for example to display correct view based on `routeData`
+  * Use `routeTail` in `<app-route>` to support child routes -> Looks very powerfull
+* Using lazy-loading
+  * `this.importHref(this.resolveUrl('./my-element.html'));`
+  * Example: `app-router`: https://github.com/erikringsmuth/app-router
+
