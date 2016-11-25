cc6eb6093f07dd9d3d22f140d94678f7e2bd6b47
Nico Jansen <jansennico@gmail.com>
2016-11-25 18:27:38 +0100

    add lazy loading

--- election-app/election-app.js
+++ election-app/election-app.js
@@ -1,62 +1,20 @@
 Polymer({
     is: 'election-app',
-    behaviors: [Polymer.NeonAnimationRunnerBehavior],
-    observers: ['_viewChanged(routeChildData.id)'],
     properties: {
-        hasWinner: {
-            type: Boolean,
-            computed: 'determineHasWinner(votes.*)'
-        },
-        animationConfig: {
-            value: function () {
-                return {
-                    vote: [{
-                        name: 'scale-down-animation',
-                        node: this.$.candidateGrid
-                    }]
-                }
-            }
-        },
+
     },
-    vote: function (event) {
-        this.push('votes', new Vote(event.model.item, new Date().toLocaleTimeString()));
-        this.playAnimation('vote');
+    observers: ['_routeChanged(routeData.view)'],
+    _routeChanged: function (view) {
+        // lazyload the page
+        this.importHref(this.resolveUrl('/' + view + '-page' + '/' + view + '-page.html'))
     },
-    sortVotes: function (a, b) {
-        return a.time < b.time;
+    goBack: function () {
+        this.set('route.path', "/voting");
     },
-    getWinner: function () {
-        var filtered = this.votes.filter(function (item) {
-            return item.candidate === this.candidates[0];
-        }.bind(this));
-        if (filtered.length == this.votes.length / 2) {
-            return 'not defined, we need one more vote';
-        } else if (filtered.length < this.votes.length / 2) {
-            return this.candidates[1].name;
-        } else {
-            return this.candidates[0].name;
-        }
+    showInfo: function (item) {
+        this.set('route.path', "/details/" + this.candidates.indexOf(item.detail));
     },
-    determineHasWinner: function () {
-        return this.votes.length > 10;
-    },
-    _showInfo: function (event) {
-        this.set('selectedCandidate', event.model.item);
-        // this.$.pages.selected = 'details';
-        this.set('route.path', '/details/' + this.candidates.indexOf(event.model.item));
-    },
-    _voteCount: function () {
-        return this.votes.filter(v => v.candidate === this.selectedCandidate).length;
-    },
-    _goBack: function () {
-        this.$.pages.selected = 'vote';
-        this.$.candidateGrid.notifyResize(); // notify to refresh... don't ask
-    },
-    _viewChanged: function (view) {
-        setTimeout(function () {
-            if (this.routeChildData.id) {
-                this.selectedCandidate = this.candidates[parseInt(this.routeChildData.id)];
-            }
-        }.bind(this));
-    },
+    vote: function (e) {
+        this.push('votes', e.detail);
+    }
 });
